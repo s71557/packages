688bb6634075410c18e42257cfc8e5093ed113b8
Stone <74167978+s71557@users.noreply.github.com>
2023-08-24 23:13:05 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -83,8 +83,8 @@ svn export https://github.com/syb999/openwrt-19.07.1/trunk/package/network/servi
 git_sparse_clone openwrt-18.06 "https://github.com/immortalwrt/luci" "luci" applications/luci-app-eqos \
 applications/luci-app-aliddns applications/luci-app-gost applications/luci-app-iptvhelper
 
-#git_sparse_clone main "https://github.com/s71557/packages" "packages" luci-app-wrtbwmon wrtbwmon luci-app-onliner \
-luci-theme-atmaterial luci-theme-atmaterial_new luci-theme-opentomcat luci-theme-opentomato luci-theme-netgear
+git_sparse_clone main "https://github.com/s71557/packages" "packages" luci-theme-argon \
+
 
 rm -rf ./*/.* & rm -rf ./*/LICENSE
 find -type f -name '*.md' -print -exec rm -rf {} \;

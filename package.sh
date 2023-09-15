5a225cb74fa049522b1534f534f5d4f13eccdeac
Stone <74167978+s71557@users.noreply.github.com>
2023-09-15 12:43:48 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -15,13 +15,13 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 
-#git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge
-#git clone --depth=1 https://github.com/kiddin9/luci-app-dnsfilter
-#git clone --depth=1 https://github.com/kiddin9/qBittorrent-Enhanced-Edition
-#git clone --depth=1 https://github.com/yaof2/luci-app-ikoolproxy
-#git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush luci-app-serverchan
-#git clone --depth=1 https://github.com/ntlf9t/luci-app-easymesh
-#git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge
+git clone --depth=1 https://github.com/kiddin9/luci-app-dnsfilter
+git clone --depth=1 https://github.com/kiddin9/qBittorrent-Enhanced-Edition
+git clone --depth=1 https://github.com/yaof2/luci-app-ikoolproxy
+git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush luci-app-serverchan
+git clone --depth=1 https://github.com/ntlf9t/luci-app-easymesh
+git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
@@ -29,7 +29,7 @@ git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb
 git clone --depth=1 https://github.com/sirpdboy/luci-app-advanced
 git clone --depth=1 https://github.com/hubbylei/luci-app-clash
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata
-#git clone --depth=1 https://github.com/thinktip/luci-theme-neobird
+git clone --depth=1 https://github.com/thinktip/luci-theme-neobird
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns
 git clone --depth=1 https://github.com/pymumu/openwrt-smartdns smartdns
@@ -40,7 +40,7 @@ git clone --depth=1 https://github.com/sensec/openwrt-udp2raw udp2raw
 git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
 git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk mentohust
 git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
 git clone --depth=1 https://github.com/fw876/helloworld && rm -rf helloworld/luci-app-ssr-plus
 
 git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter
@@ -51,16 +51,16 @@ git clone --depth 1 https://github.com/linkease/istore && mv -n istore/luci/* ./
 git clone --depth 1 https://github.com/linkease/istore-ui && mv -n istore-ui/app-store-ui ./; rm -rf istore-ui
 git clone --depth 1 https://github.com/linkease/nas-packages && mv -n nas-packages/network/services/* ./; rm -rf nas-packages
 git clone --depth 1 https://github.com/linkease/nas-packages-luci && mv -n nas-packages-luci/luci/* ./; rm -rf nas-packages-luci
-#git clone --depth 1 https://github.com/messense/aliyundrive-fuse aliyundrive && mv -n aliyundrive/openwrt/* ./; rm -rf aliyundrive
+git clone --depth 1 https://github.com/messense/aliyundrive-fuse aliyundrive && mv -n aliyundrive/openwrt/* ./; rm -rf aliyundrive
 git clone --depth 1 https://github.com/messense/aliyundrive-webdav aliyundrive && mv -n aliyundrive/openwrt/* ./; rm -rf aliyundrive
 
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
 svn export https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass
-#svn export https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic
-#svn export https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass
+svn export https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic
+svn export https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy
 svn export https://github.com/coolsnowwolf/packages/trunk/multimedia/UnblockNeteaseMusic
 svn export https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
 svn export https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom
